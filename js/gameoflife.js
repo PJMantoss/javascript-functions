56a761a0e9140eb10e63c07d08abf1f36221561e
PJMantoss <joelptoss@gmail.com>
2021-10-11 15:04:10 +0100

    update gameoflife

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -19,7 +19,11 @@ function same([x, y], [j, k]) {
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  if(this.include(cell)){
+    return
+  }
+}
 
 const printCell = (cell, state) => {};
 
