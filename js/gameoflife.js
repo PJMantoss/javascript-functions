6bec69d0b09883230c644756eb48f883c3da687b
PJMantoss <joelptoss@gmail.com>
2021-10-12 12:57:43 +0100

    update gameoflife

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -22,7 +22,7 @@ function same([x, y], [j, k]) {
 function contains(cell) {
   for(let i=0; i<cell.length; i++){
     for (let j=0; j<this.length; j++){
-      if(cell[j].includes(this[i])){
+      if(cell[j] === this[i]){
         return true;
       } else {
         return false
